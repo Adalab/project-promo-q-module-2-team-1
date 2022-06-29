896e2c36c2d6c79511adad5c5fbc0a5d73c4d09f
csuau <csuaubou@gmail.com>
2022-06-29 14:01:51 +0200

    add js avatar

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,25 @@
 
 console.log('>> Ready :)');
 
-
+//Parte colapsables
 const sectionDesign = document.querySelector('.js_section_design');
 const angleDesign = document.querySelector('.js_angle_design');
 const sectionFormDesign = document.querySelector('.js_section_form_design');
 
-function showSectionDesign() {
+function showSectionDesign () {
     sectionDesign.classList.remove('collapsed');
     angleDesign.classList.toggle('rotate');
 }
 
-function hideSectionDesign() {
+function hideSectionDesign () {
     sectionDesign.classList.add('collapsed');
     angleDesign.classList.toggle('rotate');
 }
 
-function handleClickSectionDesign(event) {
+function handleClickSectionDesign (event) {
     event.preventDefault();
 
-    if (sectionDesign.classList.contains('collapsed')){
+    if (sectionDesign.classList.contains('collapsed')) {
         showSectionDesign();
     }
     else {
@@ -30,3 +30,9 @@ function handleClickSectionDesign(event) {
 
 sectionFormDesign.addEventListener('click', handleClickSectionDesign);
 
+//parte imagen
+
+const fr = new FileReader();
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
