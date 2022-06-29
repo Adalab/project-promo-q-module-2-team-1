33cbb9b9f533e57de043da8d01e9943328f15ada
Olga RG <olga_1847@hotmail.com>
2022-06-29 18:50:24 +0200

    Add changes to styles

--- src/js/main.js
+++ src/js/main.js
@@ -8,24 +8,24 @@ const angleDesign = document.querySelector('.js_angle_design');
 const sectionFormDesign = document.querySelector('.js_section_form_design');
 
 function showSectionDesign () {
-    sectionDesign.classList.remove('collapsed');
-    angleDesign.classList.toggle('rotate');
+  sectionDesign.classList.remove('collapsed');
+  angleDesign.classList.toggle('rotate');
 }
 
 function hideSectionDesign () {
-    sectionDesign.classList.add('collapsed');
-    angleDesign.classList.toggle('rotate');
+  sectionDesign.classList.add('collapsed');
+  angleDesign.classList.toggle('rotate');
 }
 
 function handleClickSectionDesign (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (sectionDesign.classList.contains('collapsed')) {
-        showSectionDesign();
-    }
-    else {
-        hideSectionDesign();
-    }
+  if (sectionDesign.classList.contains('collapsed')) {
+    showSectionDesign();
+  }
+  else {
+    hideSectionDesign();
+  }
 }
 
 sectionFormDesign.addEventListener('click', handleClickSectionDesign);
