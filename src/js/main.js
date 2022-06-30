6b2974e668df3b4e6f48b7b9e356c2938848d52f
csuau <csuaubou@gmail.com>
2022-06-30 17:14:04 +0200

    fix toggle

--- src/js/main.js
+++ src/js/main.js
@@ -5,25 +5,25 @@ console.log('>> Ready :)');
 //Parte colapsables
 const designHeader = document.querySelector('.js_header_design');
 const designContent = document.querySelector('.js_content_design');
-const angleDesign = document.querySelector('.js_angle_design');
+//const angleDesign = document.querySelector('.js_angle_design');
 const dataHeader = document.querySelector('.js_header_data');
 const dataContent = document.querySelector('.js_content_data');
-const angleData = document.querySelector('.js_angle_data');
+//const angleData = document.querySelector('.js_angle_data');
 const shareHeader = document.querySelector('.js_header_share');
 const shareContent = document.querySelector('.js_content_share');
-const angleShare = document.querySelector('.js_angle_share');
+//const angleShare = document.querySelector('.js_angle_share');
 
 const handleClick = (ev) => {
-    const elementClick = ev.currentTarget;
-    if (elementClick.classList.contains('.js_header_design')) {
-        designContent.classList.toggle('collapsed');
-    }
-    else if (elementClick.classList.contains('.js_header_data')) {
-        dataContent.classList.toggle('collapsed');
-    }
-    else if (elementClick.classList.contains('.js_header_share')) {
-        shareContent.classList.toggle('collapsed');
-    }
+  const elementClick = ev.currentTarget;
+  if (elementClick.classList.contains('js_header_design')) {
+    designContent.classList.toggle('collapsed');
+  }
+  else if (elementClick.classList.contains('js_header_data')) {
+    dataContent.classList.toggle('collapsed');
+  }
+  else if (elementClick.classList.contains('js_header_share')) {
+    shareContent.classList.toggle('collapsed');
+  }
 };
 designHeader.addEventListener('click', handleClick);
 dataHeader.addEventListener('click', handleClick);
