f37f10eff6ef05dd8e73d59f37cbda5015b0f908
csuau <csuaubou@gmail.com>
2022-07-01 12:35:42 +0200

    add js collapsed

--- src/js/main.js
+++ src/js/main.js
@@ -5,24 +5,39 @@ console.log('>> Ready :)');
 //Parte colapsables
 const designHeader = document.querySelector('.js_header_design');
 const designContent = document.querySelector('.js_content_design');
-//const angleDesign = document.querySelector('.js_angle_design');
+const angleDesign = document.querySelector('.js_angle_design');
 const dataHeader = document.querySelector('.js_header_data');
 const dataContent = document.querySelector('.js_content_data');
-//const angleData = document.querySelector('.js_angle_data');
+const angleData = document.querySelector('.js_angle_data');
 const shareHeader = document.querySelector('.js_header_share');
 const shareContent = document.querySelector('.js_content_share');
-//const angleShare = document.querySelector('.js_angle_share');
+const angleShare = document.querySelector('.js_angle_share');
 
 const handleClick = (ev) => {
   const elementClick = ev.currentTarget;
-  if (elementClick.classList.contains('js_header_design')) {
+  if (elementClick.classList.contains('js_header_design')/* && dataContent.classList.contains('collapsed') && shareContent.classList.contains('collapsed')*/) {
     designContent.classList.toggle('collapsed');
+    dataContent.classList.add('collapsed');
+    shareContent.classList.add('collapsed');
+    angleDesign.classList.toggle('rotate');
+    angleData.classList.add('rotate');
+    angleShare.classList.add('rotate');
   }
   else if (elementClick.classList.contains('js_header_data')) {
     dataContent.classList.toggle('collapsed');
+    designContent.classList.add('collapsed');
+    shareContent.classList.add('collapsed');
+    angleData.classList.toggle('rotate');
+    angleShare.classList.add('rotate');
+    angleDesign.classList.add('rotate');
   }
   else if (elementClick.classList.contains('js_header_share')) {
     shareContent.classList.toggle('collapsed');
+    dataContent.classList.add('collapsed');
+    designContent.classList.add('collapsed');
+    angleShare.classList.toggle('rotate');
+    angleData.classList.add('rotate');
+    angleDesign.classList.add('rotate');
   }
 };
 designHeader.addEventListener('click', handleClick);
